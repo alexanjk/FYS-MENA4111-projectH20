3557ed205854a1b3bbd65e1d6105f5dc29b9dfe6
CyberMancerGamer <54441789+CyberMancerGamer@users.noreply.github.com>
2020-11-10 12:21:05 +0100

    Plotted energy convergence + organization
    Copied convergence data to /data and plotted convergence of energy, with figures in /fig

--- python/plot_energy.py
+++ python/plot_energy.py
@@ -2,9 +2,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 #filename = input("Please type the path to the filename you want to read: ")
-filename = "../data/energy-convergence.txt"
+filename = "../data/vaspout_energy_convergence.txt"
 energy = []
-job = []
+cutoff = []
 
 with open(filename) as infile:
     infile.readline()
@@ -12,19 +12,31 @@ with open(filename) as infile:
     for i,line in enumerate(lines):
         words = line.split()
         energy.append(float(words[-2]))
-        job.append(float(words[-1][:-7]))
+        cutoff.append(float(words[-1][:-7]))
 
 energy = np.array(energy)
-job = np.array(job)
+cutoff = np.array(cutoff)
 
-energy = energy/26
-print(energy)
+energy = energy/26 #TOTEN per atom
 
-plt.plot(job,energy)
-plt.plot(job,energy,"bo")
-plt.title("Energy vs. Cutoff Energy")
-plt.xlabel("Cutoff Energy [eV?]")
-plt.ylabel("Energy per atom [eV]")
+plt.plot(cutoff,energy,"b-")
+plt.plot(cutoff,energy,"bo")
+plt.title("Convergence of energy")
+plt.xlabel("Cutoff Energy [eV]")
+plt.ylabel("Total Energy per atom [eV/atom]")
 plt.grid()
-#plt.legend()
+plt.tight_layout()
+plt.savefig("../fig/vaspout_energy_convergence.png")
+plt.show()
+
+energy_difference = abs(np.diff(energy))
+
+plt.plot(cutoff[:-1],energy_difference,"b-")
+plt.plot(cutoff[:-1],energy_difference,"bo")
+plt.title("Energy difference")
+plt.xlabel("Cutoff Energy [eV]")
+plt.ylabel("Absolute Total Energy Difference per atom [eV/atom]")
+plt.grid()
+plt.tight_layout()
+plt.savefig("../fig/vaspout_energy_difference_convergence.png")
 plt.show()
