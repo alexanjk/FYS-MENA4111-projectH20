130e55564a4cc7c8e3616642a4852b5808f20cb8
CyberMancerGamer <54441789+CyberMancerGamer@users.noreply.github.com>
2020-11-10 12:35:06 +0100

    plotted kpints convergence and renamed figures and data-files
    plotted kpints convergence and renamed figures and data-files

--- python/plot_kpoints.py
+++ python/plot_kpoints.py
@@ -2,9 +2,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 #filename = input("Please type the path to the filename you want to read: ")
-filename = "../data/convergence_energy.txt"
+filename = "../data/convergence_kpoints.txt"
 energy = []
-cutoff = []
+kpoint = []
 
 with open(filename) as infile:
     infile.readline()
@@ -12,31 +12,31 @@ with open(filename) as infile:
     for i,line in enumerate(lines):
         words = line.split()
         energy.append(float(words[-2]))
-        cutoff.append(float(words[-1][:-7]))
+        kpoint.append(float(words[-1][:-7]))
 
 energy = np.array(energy)
-cutoff = np.array(cutoff)
+kpoint = np.array(kpoint)
 
 energy = energy/26 #TOTEN per atom
 
-plt.plot(cutoff,energy,"b-")
-plt.plot(cutoff,energy,"bo")
-plt.title("Convergence of energy by energy cutoff")
-plt.xlabel("Cutoff Energy [eV]")
+plt.plot(kpoint,energy,"b-")
+plt.plot(kpoint,energy,"bo")
+plt.title("Convergence of energy by kpoints")
+plt.xlabel("Kpoint density [a.u.]")
 plt.ylabel("Total Energy per atom [eV/atom]")
 plt.grid()
 plt.tight_layout()
-plt.savefig("../fig/convergence_energy.png")
+plt.savefig("../fig/convergence_kpoints.png")
 plt.show()
 
 energy_difference = abs(np.diff(energy))
 
-plt.plot(cutoff[:-1],energy_difference,"b-")
-plt.plot(cutoff[:-1],energy_difference,"bo")
-plt.title("Convergence of energy by energy cutoff - difference")
-plt.xlabel("Cutoff Energy [eV]")
+plt.plot(kpoint[:-1],energy_difference,"b-")
+plt.plot(kpoint[:-1],energy_difference,"bo")
+plt.title("Convergence of energy by kpoints - difference")
+plt.xlabel("Kpoint density [a.u.]")
 plt.ylabel("Absolute Total Energy Difference per atom [eV/atom]")
 plt.grid()
 plt.tight_layout()
-plt.savefig("../fig/convergence_energy_difference.png")
+plt.savefig("../fig/vaspout_convergence_kpoints_difference.png")
 plt.show()
